57bd3807eb14a46d3db421045f8b1295c509a788
Zhunus <35253183+zhunus1@users.noreply.github.com>
2018-12-13 14:17:58 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="main.css">
+	<script src="script.js" defer></script>
+</head>
+<body>
+	<header>
+		<div class="header">
+			<div id="name">
+				<p>Johny's Garage</p>
+			</div>
+			<div id="links">
+				<nav>
+					<a href="">About us</a>
+					<a href="">Contacts</a>
+					<a href="">Personal</a>
+					<a href="">FeedBack</a>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<section>
+			<div class="view">
+				<div id="text1"><h1>Добро пожаловать в Johny's Garage!</h1></div>
+				<div id="text2"><p>Ниже вы можете найти свой автомобиль мечты!</p></div>
+			</div>
+	</section>
+		<section>
+			<div class="part1">
+				<div class="container">
+				<div class="line1">
+					<div class="text">
+						<h1>Искать автомобиль по:</h1>
+					</div>
+					<div class="types">
+						<button id="par1" value="true" onclick="price()">Цене и году выпуска</button>
+						<button id="par2" value="true" onclick="parametr()">По всем параметрам</button>
+				</div>
+				</div>
+					<div class="line2">
+						<div id="brands">
+							<div id="mark">
+								<p>Выберите марку:</p>
+								<select id="brandy"><option>Choose</option></select>
+							</div>
+							<div id="model">
+								<p>Выберите модель:</p>
+								<select id="modeli"><option>Choose</option></select>
+							</div>
+						</div>
+						<div id="blocks">
+						<div id="price">
+							<p>Цена:</p>
+							<div id="line1">
+								<input type="text" name="price1" placeholder=" от">
+								<input type="text" name="price2" placeholder=" до">
+							</div>
+						</div>
+						<div id="year">
+							<p>Год выпуска:</p>
+							<div id="line2">
+								<input type="text" name="year" placeholder=" от">
+								<input type="text" name="year" placeholder=" до">
+							</div>
+						</div>
+						</div>
+					</div>
+				<div class="find">
+				<button>Искать!</button>
+			</div>
+			</div>
+		</div>
+		</section>
+		<section>
+			<div class="cards">	
+			
+
+
+
+			</div>
+		</section>
+	<footer></footer>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,96 @@
+function price() {
+    var x = document.getElementById("brands");
+    var z=document.getElementById("par1");
+    var y=document.getElementById("par2");
+        x.style.display = "none";
+        z.value="true";
+        y.value="false";
+        
+}
+//------------------------------------------------------------
+function parametr() {
+    var x = document.getElementById("brands");
+    var z=document.getElementById("par1");
+    var y=document.getElementById("par2");
+        x.style.display = "block"; 
+        z.value="false";
+        y.value="true";
+}
+//------------------------------------------------------------
+let brandss = ["Mercedes-Benz","Lexus","Rolls&Royce","BMW","Lamborghini","Ferrari"];
+let modelss = {"Mercedes-Benz":["S 400","S63 AMG","S65 AMG"],"Lexus":["LS500","RX350h","NX200t"],"Rolls&Royce":["Wraith","Ghost","Phantom"],"Lamborghini":["Aventador","Huracan"],"BMW":["M5","760Li","X6M"],"Ferrari":["458 Italia","FF","California"]};
+function f() {
+    var q = document.getElementById('modeli');
+    for (let i in q){
+    	q.remove(i);}
+    for (let i in modelss){
+    	if(event.target.value ==  i){
+    		for(let j of modelss[i]){
+    			a1 = document.createElement("option");
+                a1.text = j;
+                q.add(a1);}}}}
+	var c = document.getElementById('brandy');
+	for (let i of brandss){
+    	ad = document.createElement("option");
+    	ad.text = i;
+    	c.add(ad);
+	}
+document.getElementById('brandy').addEventListener('click',f);
+//Getting data from form!
+//----------------------------------------------------------
+let car = {};
+function getInformFull(){
+	car["brand"]=document.getElementById("brandy").value;
+	car["model"]=document.getElementById("modeli").value;
+	car["pricefrom"]=document.getElementsByName("price1")[0].value;
+	car["priceto"]=document.getElementsByName("price2")[0].value;
+	car["yearfrom"]=document.getElementsByName("year")[0].value;
+	car["yearto"]=document.getElementsByName("year")[1].value;
+    
+}
+
+function getInformPart(){
+	car["pricefrom"]=document.getElementsByName("price1")[0].value;
+	car["priceto"]=document.getElementsByName("price2")[0].value;
+	car["yearfrom"]=document.getElementsByName("year")[0].value;
+	car["yearto"]=document.getElementsByName("year")[1].value;
+
+}
+//--------------------------------------------------------
+function checking(){
+	if(document.getElementById("par1").value=="false"){
+		getInformFull();
+	}else if(document.getElementById("par2").value=="false"){
+		getInformPart();
+	}else{
+	   getInformFull();
+	}
+}
+
+document.getElementsByTagName("button")[2].addEventListener('click',checking);
+//-------------------------------------------------------
+//Finishing getting data from form!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------
+//Finishing accessing to DataBase
